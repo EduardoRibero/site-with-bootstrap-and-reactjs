ed0a1e7a8c0760927824381624f2cad3348f7926
EduardoRibero <ribeirodomingues2020@gmail.com>
2024-07-11 11:40:09 -0300

    Start project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Logo">
+          <img src="" alt="logoImg" />
+        </div>
+        <nav>
+          <a href="sdad">Home</a>
+          <a href="asd">Sobre</a>
+          <a href="asd">Duvidas</a>
+          <a href="asd">Contatos</a>
+          <a href="asd">Orçamento</a>
+        </nav>
+      </header>
+      <main>
+        <div className="Apresentacao">
+          <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam architecto velit odit, eveniet vitae at facere harum rerum laboriosam recusandae temporibus quo consectetur qui laborum suscipit accusamus numquam laudantium debitis.</h1>
+          <img src="https://blog.parceirodaconstrucao.com.br/wp-content/uploads/2022/05/iStock-1049404090.jpg" alt="HomeImg"/>
+        </div>
+        <div className="Servicos">
+          
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
