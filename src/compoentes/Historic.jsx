5a2c6603c53a6573c9b6ef34039357f8e15ae71e
EduardoRibero <ribeirodomingues2020@gmail.com>
2024-07-18 16:01:30 -0300

    page doubts

--- src/compoentes/Historic.jsx
+++ src/compoentes/Historic.jsx
@@ -9,7 +9,7 @@ const Historic = () => {
                     src="https://civilizacaoengenheira.wordpress.com/wp-content/uploads/2023/06/image.png?w=886"
                     alt="First slide"
                 />
-                <Carousel.Caption className='text-white'>
+                <Carousel.Caption>
                     <h5>First slide label</h5>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
@@ -20,7 +20,7 @@ const Historic = () => {
                     src="https://images.adsttc.com/media/images/623c/c6ff/3e4b/3129/8900/0004/medium_jpg/PERI_5.jpg?1648150264"
                     alt="Second slide"
                 />
-                <Carousel.Caption className='text-white'>
+                <Carousel.Caption>
                     <h5>Second slide label</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
@@ -31,7 +31,7 @@ const Historic = () => {
                     src="https://images01.brasildefato.com.br/fedee6a7daed165617f79c4b1e11d76b.jpeg"
                     alt="Third slide"
                 />
-                <Carousel.Caption className='text-white'>
+                <Carousel.Caption>
                     <h5>Third slide label</h5>
                     <p>
                         Praesent commodo cursus magna, vel scelerisque nisl consectetur.
--- src/pages/Doubts.jsx
+++ src/pages/Doubts.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { Container, Row, Col } from "react-bootstrap"
+import Acordion from "../compoentes/Acordion"
 
 const Doubts = () => {
   return (
-    <div>Doubts</div>
+    <div>
+      <Container>
+        <Row>
+          <Col className="text-center m-5 p-5">
+            <h1>
+              Duvidas frequentes
+            </h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Acordion></Acordion>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
